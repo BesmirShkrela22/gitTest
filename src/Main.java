8de82ef6fc0d88ad90f711b8c46b784921d4adee
BesmirShkrela22 <besmirshkrela23@gmail.com>
2023-10-24 22:19:08 +0200

    from AJNA

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello from master");
         System.out.println("hello from devbesi ");
-        System.out.println("hello from dev1 ");
         System.out.println("hello from AJNA");
 
 
