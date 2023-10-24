0c58167553d8e6faea7dfb7391db92cc592fab49
BesmirShkrela22 <besmirshkrela23@gmail.com>
2023-10-24 20:29:10 +0200

    first commit from besi

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello from ajna");
+        System.out.println("hello from besi");
+        System.out.println("hello from bora");
 
     }
 }
