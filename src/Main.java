3d3e6b3d0f2a11be2f9f11e8a895ef5b9830d854
BesmirShkrela22 <besmirshkrela23@gmail.com>
2023-10-24 19:42:49 +0200

    first commit develop5

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello branch1");
-        System.out.println("Hello from develop1");
-        System.out.println("Hello from develop22");
+        System.out.println("Hello branch5");
+        System.out.println("Hello branch555");
+        System.out.println("Hello branch55");
+
     }
 }
