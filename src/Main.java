fc4855570d7ed30da5e97e258ab95062b58166e7
BesmirShkrela22 <besmirshkrela23@gmail.com>
2023-10-24 22:47:29 +0200

    from BABI

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello from master");
-        System.out.println("hello from devbesiXXX ");
-        System.out.println("hello from AJNAXXXX");
+        System.out.println("hello from BABI");
+
 
 
     }
