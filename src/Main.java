6b4fe27abf54ba0bea248c30b0d5183bb24a132b
BesmirShkrela22 <besmirshkrela23@gmail.com>
2023-10-24 19:48:40 +0200

    kthim prap te 5 versioni 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello branch5");
+        System.out.println("Hello branch555");
+        System.out.println("Hello branch55");
+
         System.out.println("Hello branch1");
         System.out.println("Hello from develop1");
         System.out.println("Hello from develop2");
