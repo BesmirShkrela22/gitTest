a0026ec0c4ca27d1327925d2ce460566f6eb29d6
BesmirShkrela22 <besmirshkrela23@gmail.com>
2023-10-24 19:05:43 +0200

    first commit from develop2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello branch1");
         System.out.println("Hello from develop1");
+        System.out.println("Hello from develop2");
     }
 }
