801b1ee2afcaadc65e9fb9da2c7bfc573cf4ff93
BesmirShkrela22 <besmirshkrela23@gmail.com>
2023-10-25 18:37:37 +0200

    ajna

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("MASTER1");
+        System.out.println("AJNA");
         System.out.println("BORA1");
         System.out.println("BESI");
 
