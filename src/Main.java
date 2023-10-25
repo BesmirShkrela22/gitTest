b17fc58acaa08143ae3a5d98cc2c1d97489f3a78
BesmirShkrela22 <besmirshkrela23@gmail.com>
2023-10-25 19:17:45 +0200

    BORA

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("BORA1");
-        System.out.println("BESI");
+        System.out.println("BORA");
+        System.out.println("BORA");
+        System.out.println("BORA");
+        System.out.println("BORA");
+        System.out.println("BORA");
+
 
 
 
